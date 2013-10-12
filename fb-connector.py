493bf938192eb51eb0e8c1a4043c8f9014d78408
Bryan Tran <btran@Bryans-MacBook-Air.local>
2013-10-11 22:54:43 -0700

    git push stuff

--- fb-connector.py
+++ fb-connector.py
@@ -1,26 +1,24 @@
 """Facebook connector glue stuff."""
 from flask import Flask, request
 import requests
-from urllib import urlencode
 app = Flask(__name__)
 
 app_id = '599387690117256'
 app_secret = '287cac91b8ee347d1325a9dffd20fc2b'
 my_uri = 'http://www.wonderboltseven.com:5000/fb-login'
-
+my_uri_encoded = 'http%3A%2F%2Fwww.wonderboltseven.com%3A5000%2Ffb-login'
 
 @app.route('/fb-login', methods=['GET', 'POST'])
 def get_oauth_token():
 	if 'code' in request.args:
-		#return process_oauth_token(request.args['code'])
-		return request.args['code']
+		return process_oauth_token(request.args['code'])[12:]
 	else:
 		return fb_oauth_redirect()
 
 def process_oauth_token(code, s=requests.Session()):
 	token_url = 'https://graph.facebook.com/oauth/access_token?client_id={0}' +\
-				'&redirect_uri={1}&client_secret={2}&code{3}'
-	url = token_url.format(app_id, urlencode(my_uri), app_secret, str(code))
+				'&redirect_uri={1}&client_secret={2}&code={3}'
+	url = token_url.format(app_id, my_uri_encoded, app_secret, code)
 	return s.get(url).text
 
 
