fd766854a1cac27c1acd3a8023a8441554b2508d
Brandon Wu <bwu@berkeley.edu>
2013-10-11 22:25:00 -0700

    calmness.

--- fb-connector.py
+++ fb-connector.py
@@ -2,6 +2,7 @@
 from flask import Flask, request
 import requests
 from urllib import urlencode
+from urllib import request as urllib_request
 app = Flask(__name__)
 
 app_id = '599387690117256'
@@ -12,16 +13,16 @@ my_uri = 'http://www.wonderboltseven.com:5000/fb-login'
 @app.route('/fb-login', methods=['GET', 'POST'])
 def get_oauth_token():
 	if 'code' in request.args:
-		#return process_oauth_token(request.args['code'])
-		return request.args['code']
+		return process_oauth_token(request.args['code'])
+		#return request.args['code']
 	else:
 		return fb_oauth_redirect()
 
-def process_oauth_token(code, s=requests.Session()):
+def process_oauth_token(code):
 	token_url = 'https://graph.facebook.com/oauth/access_token?client_id={0}' +\
 				'&redirect_uri={1}&client_secret={2}&code{3}'
 	url = token_url.format(app_id, urlencode(my_uri), app_secret, str(code))
-	return s.get(url).text
+	return urllib_request.urlopen(url).read()
 
 
 def fb_oauth_redirect():
