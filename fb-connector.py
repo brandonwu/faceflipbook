6d2f1d19a0d9fcc8da8b11862528c4b2c3d78f6b
Brandon Wu <bwu@berkeley.edu>
2013-10-11 22:29:48 -0700

    holy hell what did i do

--- fb-connector.py
+++ fb-connector.py
@@ -2,27 +2,27 @@
 from flask import Flask, request
 import requests
 from urllib import urlencode
-import urllib2
+from urllib import request as urllib_request
 app = Flask(__name__)
 
 app_id = '599387690117256'
 app_secret = '287cac91b8ee347d1325a9dffd20fc2b'
-my_uri = 'http://www.wonderboltseven.com:5000/fb-login'
-
+my_uri = 'http://www.wonderboltseven.com:5000/fb-loginhttp%3A%2F%2Fwww.wonderboltseven.com%3A5000%2Ffb-login'
+my_uri_encoded = ''
 
 @app.route('/fb-login', methods=['GET', 'POST'])
 def get_oauth_token():
 	if 'code' in request.args:
-		return process_oauth_token(request.args['code'])
-		#return request.args['code']
+		#return process_oauth_token(request.args['code'])
+		return request.args['code']
 	else:
 		return fb_oauth_redirect()
 
 def process_oauth_token(code):
 	token_url = 'https://graph.facebook.com/oauth/access_token?client_id={0}' +\
 				'&redirect_uri={1}&client_secret={2}&code{3}'
-	url = token_url.format(app_id, urlencode(my_uri), app_secret, str(code))
-	return urllib2.urlopen(url).read()
+	url = token_url.format(app_id, my_uri_encoded, app_secret, code)
+	return s.get(url).text
 
 
 def fb_oauth_redirect():
